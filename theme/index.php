8d494a55225dc203161827f9bdc8d15d1748c26c
zerokhan <a.zhakfar@gmail.com>
2017-03-28 00:01:57 +0430

    Add files via upload

---
+++ theme/index.php
@@ -0,0 +1,84 @@
+  <!DOCTYPE html>
+            <html lang="en">
+            <head>
+            <!-- Required meta tags -->
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+            <!-- Affixshape CSS -->
+            <link rel="stylesheet" href="../default.css" >
+            </head>
+            <body>
+        
+		
+		
+		 <div class="navbar">
+        <div class="navbar-name">Navbar</div>
+        <ul class="navbar-menu">
+            <li class="navbar-menu-item"><a href='#'>Home</a></li>
+            <li class="navbar-menu-item"><a href='#'>About us</a></li>
+            <li class="navbar-menu-item"><a href='#'>Contact us</a></li>
+            <li class="navbar-menu-item disabled"><a class="disabled" href='#'>Disable</a></li>
+        </ul>
+    </div>
+	
+	
+	
+        <div class="col-9 wraps p-10">  
+            
+        <h1>Hello, world!</h1>
+
+        <div class="row p-10">                
+        <div class="card">
+            <div class="card-header">Header</div>
+            <div class="card-content">
+            <h1>Title</h1>
+            <p>With supporting text below as a natural lead-in to additional content.</p>
+            <button class="btn btn-basic">More</button>
+            </div>
+        </div>
+        </div>
+                
+                
+        <div class="row">
+        <div class="col-4 float-left p-10">
+        <div class="card">
+            <div class="card-header">Header</div>
+            <div class="card-content">
+            <h1>Title</h1>
+            <p>With supporting text below as a natural lead-in to additional content.</p>
+            <button class="btn btn-basic">More</button>
+            </div>
+        </div>
+        </div>   
+        
+        <div class="col-4 float-left p-10">
+        <div class="card">
+            <div class="card-header">Header</div>
+            <div class="card-content">
+            <h1>Title</h1>
+            <p>With supporting text below as a natural lead-in to additional content.</p>
+            <button class="btn btn-basic">More</button>
+            </div>
+        </div>
+        </div>
+                
+        <div class="col-4 float-left p-10">        
+        <div class="card">
+            <div class="card-header">Header</div>
+            <div class="card-content">
+            <h1>Title</h1>
+            <p>With supporting text below as a natural lead-in to additional content.</p>
+            <button class="btn btn-basic">More</button>
+            </div>
+        </div>
+        </div>
+            
+        </div>
+            
+            
+            </div>
+
+            </body>
+            </html>
+                
